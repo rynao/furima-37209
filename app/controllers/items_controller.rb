5b5f4d15ff685a2337053115725841d29abf7cb9
rynao <94769118+rynao@users.noreply.github.com>
2021-12-16 15:21:01 +0900

    エラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to :index
+      redirect_to root_path
     else
       render :new
     end
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :ship_cost_id, :prefecture_id, :ship_date_id).merge(user_id: current_user)
+    params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :ship_cost_id, :prefecture_id, :ship_date_id, :image).merge(user_id: current_user.id)
   end
 
 end
