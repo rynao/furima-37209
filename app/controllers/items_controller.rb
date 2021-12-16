45b226851981a859e2e09e4efa249f022b56a5af
rynao <94769118+rynao@users.noreply.github.com>
2021-12-16 14:35:38 +0900

    Active Hashインストール、商品出品画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :ship_cost_id, :prefecture_id, :ship_date_id).merge(user_id: current_user)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
 
   NUMBER_REGEX = /\A[0-9]+\z/.freeze
   validates_format_of :price, with: NUMBER_REGEX
-  
+
   validates :price, numericality: { in: 300..9999999 }
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :ship_cost
