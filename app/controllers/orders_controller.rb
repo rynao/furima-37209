61ee75fe8d9221b0f4477a3b3c850ff8a9a6b1c4
rynao <94769118+rynao@users.noreply.github.com>
2021-12-22 10:24:40 +0900

    Formオブジェクトを用いた購入情報の登録機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address_line, :building, :telephone).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -10,15 +10,18 @@ class PurchaseAddress
     validates :city
     validates :address_line
     validates :telephone
-    validates :purchase_id
     # validates :token, message: "is invalid.Credit card data can't be blank"
 
   end
 
   POST_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/.freeze
   TELEPHONE_REGEX = /\A\d{10,11}\z/.freeze
-  validates :postal_code, format: { with: POST_CODE_REGEX, message: "is invalid. Include hyphen(-)" }
+  validates :post_code, format: { with: POST_CODE_REGEX, message: "is invalid. Include hyphen(-)" }
   validates :telephone, numericality: { with: TELEPHONE_REGEX, message: "is invalid. Input only number" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building: building, telephone: telephone, purchase_id: purchase.id)
+  end
 end
