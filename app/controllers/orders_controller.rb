dc1403836d735487cca79b6b7ac9f59cbc4363b7
rynao <94769118+rynao@users.noreply.github.com>
2021-12-22 10:44:27 +0900

    ログイン状態による条件分岐、自身の出品か否かによる条件分岐実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_params_user_confirm
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
@@ -18,5 +18,12 @@ class OrdersController < ApplicationController
   private
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address_line, :building, :telephone).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+  def find_params_user_confirm
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
